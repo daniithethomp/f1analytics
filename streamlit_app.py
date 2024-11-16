784e02f3b59e2ab8f19507507777369f5e1a45ba
Danii Thompson <109387187+daniithethomp@users.noreply.github.com>
2024-11-16 14:36:25 +0000

    getting data from dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import pandas as pd
+import kagglehub
 
-st.title("🎈 My new app")
+# Download latest version
+path = kagglehub.dataset_download("rohanrao/formula-1-world-championship-1950-2020")
+
+print("Path to dataset files:", path)
+
+df_drivers = pd.read_csv(path + "/drivers.csv")
+df_drivers['full_name'] = df_drivers['forename'] + " " + df_drivers['surname']
+print(df_drivers['full_name'])
+
+st.title("F1 Growth Thing Name")
+st.selectbox("Drivers",
+             options=([name for name in df_drivers['full_name']]))
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    ""
 )
