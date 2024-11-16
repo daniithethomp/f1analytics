ca77dbe21bc5a965516db11e444f0ae6cee37870
Danii Thompson <109387187+daniithethomp@users.noreply.github.com>
2024-11-16 20:46:40 +0000

    fastest lap per race

--- analytics.py
+++ analytics.py
@@ -17,3 +17,16 @@ def constructor_standings_over_time(constructor):
     constructor_specific_standings = constructor_specific_standings.join(df.races_df(), on='raceId', lsuffix='l', rsuffix='r')
     constructor_standing_per_year = constructor_specific_standings.loc[constructor_specific_standings.groupby('year')['round'].idxmax()]
     return constructor_standing_per_year
+
+def driver_wins_over_time(driver):
+    drivers_df = df.drivers_df()
+    driver_id = (drivers_df)[drivers_df['Full Name'] == driver]['driverId'].iloc[0]
+    driver_wins_frame = df.driver_standings_df
+    driver_specific_wins = driver_wins_frame[driver_wins_frame['driverId'] == driver_id]
+    driver_specific_wins = driver_specific_wins.join(df.races_df(), on='raceId', lsuffix='l', rsuffix='r')
+    driver_wins_per_year = driver_specific_wins[driver_specific_wins['position'] == 1].groupby('year').size()
+    return driver_wins_per_year
+
+results = df.results_df
+fastest_laps = results[results['fastestLapTime'].notnull()]
+fastest_laps = fastest_laps.groupby('raceId')['fastestLapTime'].min()
